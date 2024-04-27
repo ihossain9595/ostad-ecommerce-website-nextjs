2191b40d035e92b164a09da3bf1a75a499567a8a
Ismail Hossain <ismail.9595@gmail.com>
2024-04-27 18:22:58 +0600

    create brand list, category list, slider list apis

--- src/app/api/product/brand-list/route.js
+++ src/app/api/product/brand-list/route.js
@@ -1,1 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
 
+export async function GET(request, response) {
+  try {
+    const prisma = new PrismaClient();
+    const result = await prisma.brands.findMany();
+    return NextResponse.json({ status: "success", data: result });
+  } catch (error) {
+    return NextResponse.json({ status: "fail", message: error });
+  }
+}
