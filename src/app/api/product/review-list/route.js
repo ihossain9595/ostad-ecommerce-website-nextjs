40e297f9ba8fb8140b73d43413049f04701001b9
Ismail Hossain <ismail.9595@gmail.com>
2024-04-27 18:35:17 +0600

    create details, review list apis

--- src/app/api/product/review-list/route.js
+++ src/app/api/product/review-list/route.js
@@ -1,1 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
 
+export async function GET(request, response) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const id = parseInt(searchParams.get("id"));
+
+    const prisma = new PrismaClient();
+    const result = await prisma.product_reviews.findMany({ where: { product_id: id }, include: { customer_profiles: { select: { cus_name: true } } } });
+    return NextResponse.json({ status: "success", data: result });
+  } catch (error) {
+    return NextResponse.json({ status: "fail", message: error });
+  }
+}
