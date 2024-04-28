4e6a315a3a2682e44d82e182708681172051f7b5
Ismail Hossain <ismail.9595@gmail.com>
2024-04-28 06:54:36 +0600

    create middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,25 @@
-function middleware(req, res, next) {}
+import { NextResponse } from "next/server";
+import { VerifyToken } from "./utility/JWTTokenHelper";
 
-export default middleware;
+export async function middleware(request, response) {
+  try {
+    const token = request.cookies.get("token");
+    const payload = await VerifyToken(token["value"]);
+
+    const requestHeader = new Headers(request.headers);
+    requestHeader.set("email", payload["email"]);
+    requestHeader.set("id", payload["id"]);
+
+    return NextResponse.next({ request: { headers: requestHeader } });
+  } catch (error) {
+    if (request.url.startsWith("/api/")) {
+      return NextResponse.json({ status: "fail", data: "Unauthorized" }, { status: 401 });
+    } else {
+      response.redirect("/login");
+    }
+  }
+}
+
+export const config = {
+  matcher: ["/api/cart/:path*", "/api/invoice/:path*", "/api/user/profile", "/api/wish/:path*"],
+};
