c1e1c2b46c476c95de2daf24a91a5154f3a0678a
Ismail Hossain <ismail.9595@gmail.com>
2024-04-28 06:31:00 +0600

    create jwt token helper function and verify otp api

--- src/app/api/user/verify-otp/route.js
+++ src/app/api/user/verify-otp/route.js
@@ -1,1 +1,26 @@
+import { CreateToken } from "@/utility/JWTTokenHelper";
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
 
+export async function POST(request, response) {
+  try {
+    const requestBody = await request.json();
+
+    const prisma = new PrismaClient();
+    const result = await prisma.users.findMany({ where: requestBody });
+
+    if (result === 0) {
+      return NextResponse.json({ status: "fail", data: "Invalid Verification Code" });
+    } else {
+      await prisma.users.update({ where: { email: requestBody["email"] }, data: { otp: "0" } });
+
+      const token = await CreateToken(result[0].id, result[0].email);
+      const expireDuration = new Date(Date.now() + 24 * 60 * 60 * 1000);
+      const cookieString = `token=${token}; expires=${expireDuration}; path=/`;
+
+      return NextResponse.json({ status: "success", data: token }, { status: 200, headers: { "set-cookie": cookieString } });
+    }
+  } catch (error) {
+    return NextResponse.json({ status: "fail", data: error });
+  }
+}
---
+++ src/utility/JWTTokenHelper.js
@@ -0,0 +1,17 @@
+import { SignJWT, jwtVerify } from "jose";
+
+export async function CreateToken(id, email) {
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+  const payload = { id: id, email: email };
+
+  const token = await new SignJWT(payload).setExpirationTime(process.env.JWT_EXPIRATION_TIME).setIssuedAt().setIssuer(process.env.JWT_ISSUER).setProtectedHeader({ alg: "HS256" }).sign(secret);
+
+  return token;
+}
+
+export async function VerifyToken(token) {
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+  const decoded = await new jwtVerify(token, secret);
+  return decoded["payload"];
+}
