7d05a32856a3445d242a476b096561ccf5c3b40e
Ismail Hossain <ismail.9595@gmail.com>
2024-04-29 06:01:06 +0600

    create wish list apis

--- src/app/api/wish/list/route.js
+++ src/app/api/wish/list/route.js
@@ -1,1 +1,49 @@
+import { PrismaClient } from "@prisma/client";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
 
+export async function POST(request, response) {
+  try {
+    const headerList = headers();
+    const id = parseInt(headerList.get("id"));
+
+    const requestBody = await request.json();
+
+    const prisma = new PrismaClient();
+    const result = await prisma.product_wishes.create({ data: { product_id: requestBody["product_id"], user_id: id } });
+
+    return NextResponse.json({ status: "success", data: result });
+  } catch (error) {
+    return NextResponse.json({ status: "fail", data: error });
+  }
+}
+
+export async function DELETE(request, response) {
+  try {
+    const headerList = headers();
+    const id = parseInt(headerList.get("id"));
+
+    const requestBody = await request.json();
+
+    const prisma = new PrismaClient();
+    const result = await prisma.product_wishes.deleteMany({ where: { AND: [{ product_id: requestBody["product_id"] }, { user_id: id }] } });
+
+    return NextResponse.json({ status: "success", data: result });
+  } catch (error) {
+    return NextResponse.json({ status: "fail", data: error });
+  }
+}
+
+export async function GET(request, response) {
+  try {
+    const headerList = headers();
+    const id = parseInt(headerList.get("id"));
+
+    const prisma = new PrismaClient();
+    const result = await prisma.product_wishes.findMany({ where: { user_id: id }, include: { products: true } });
+
+    return NextResponse.json({ status: "success", data: result });
+  } catch (error) {
+    return NextResponse.json({ status: "fail", data: error });
+  }
+}
