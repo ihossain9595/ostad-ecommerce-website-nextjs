1a9485ab513406527638fc5878f1ab13f0867cb3
Ismail Hossain <ismail.9595@gmail.com>
2024-05-01 00:48:01 +0600

    create payment success, fail, cancel and ipn apis

--- src/app/api/payment/payment-success/route.js
+++ src/app/api/payment/payment-success/route.js
@@ -1,1 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
 
+export async function POST(request, response) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const tran_id = searchParams.get("tran_id");
+
+    const prisma = new PrismaClient();
+    const result = await prisma.invoices.updateMany({ where: { AND: [{ tran_id: tran_id }] }, data: { payment_status: "success" } });
+
+    return NextResponse.json({ status: "success", data: result });
+  } catch (error) {
+    return NextResponse.json({ status: "fail", data: error });
+  }
+}
