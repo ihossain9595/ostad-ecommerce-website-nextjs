26f55774687928175b6328edf423b47a1899b3b0
Ismail Hossain <ismail.9595@gmail.com>
2024-05-01 00:48:19 +0600

    create logout api

--- src/app/api/user/logout/route.js
+++ src/app/api/user/logout/route.js
@@ -1,1 +1,7 @@
+import { NextResponse } from "next/server";
 
+export async function GET(request, response) {
+  let expireDuration = new Date(Date.now() - 24 * 60 * 60 * 1000);
+  const cookieStrings = `token=' '; expires= ${expireDuration.toUTCString()}; path=/`;
+  return NextResponse.json({ status: "success" }, { status: 200, headers: { "set-cookie": cookieStrings } });
+}
