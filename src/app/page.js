064f437655af483673519b575b3390e13a250021
Ismail Hossain <ismail.9595@gmail.com>
2024-05-01 13:40:43 +0600

    add skeleton components

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,19 @@
+import Master from "@/components/master/Master";
+import BrandsSkeleton from "@/skeleton/brands-skeleton";
+import CategoriesSkeleton from "@/skeleton/categories-skeleton";
+import FeaturesSkeleton from "@/skeleton/features-skeleton";
+import ProductsSkeleton from "@/skeleton/products-skeleton";
+import SliderSkeleton from "@/skeleton/slider-skeleton";
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <Master>
+      <SliderSkeleton />
+      <FeaturesSkeleton />
+      <BrandsSkeleton />
+      <ProductsSkeleton />
+      <CategoriesSkeleton />
+    </Master>
+  );
 }
